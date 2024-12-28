12ff26a737dff5e7429d62a811510a9e7b83729b
dariadeev <dariadeev@gmail.com>
2024-12-28 22:21:53 +0200

    seaborn add

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-st.markdown("""
 
-# Title
-## Subtitle
+# st.markdown("""
+#
+# # Title
+# ## Subtitle
+#
+# - bullet 1
+# - bullet 2
+# - bullet 3
+#
+# > Amazing Quote
+#
+# """)
+#
+# st.radio("Which dessert is best?", ["Cake", "Ice Cream", "Pie"])
 
-- bullet 1
-- bullet 2
-- bullet 3
+df = sns.load_dataset("penguins") #load dataset
 
-> Amazing Quote
+fig, ax = plt.subplots() # Create a new figure, get the axes object
+sns.scatterplot(data=df, x="flipper_length_mm", y="bill_length_mm", hue="species",
+                ax=ax) #Use the axes object to plot on the same figure
 
-""")
+st.pyplot(fig) #show the figure in Streamlit
 
-st.radio("Which dessert is best?", ["Cake", "Ice Cream", "Pie"])
+
 
 
